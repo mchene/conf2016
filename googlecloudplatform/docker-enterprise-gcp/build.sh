f6df9a7fa22217572f5f9e86bb5ef8fa6bae26ae
mchene <mchene@splunk.com>
2016-09-28 10:39:20 -0400

    Adding GCP App and Dashboards.

---
+++ googlecloudplatform/docker-enterprise-gcp/build.sh
@@ -0,0 +1,6 @@
+#https://docs.docker.com/engine/reference/commandline/build/
+if [ -z $CURRENT ]; then
+	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+fi
+
+docker build --no-cache=true -t mchene/enterprise:6.4.3-monitor-gcp $CURRENT
